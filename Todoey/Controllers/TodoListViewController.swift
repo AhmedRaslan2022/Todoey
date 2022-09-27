e8a95e4e6b8b3459eb011e9797a4a1edd80517d1
AhmedRaslan2022 <a.rasslan01@gmail.com>
2022-09-27 09:53:25 +0200

    adding searchBar imp

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -72,8 +72,8 @@ extension TodoListViewController{
         
         override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             
-            context.delete(itemArray[indexPath.row])
-            itemArray.remove(at: indexPath.row)
+//            context.delete(itemArray[indexPath.row])
+//            itemArray.remove(at: indexPath.row)
             itemArray[indexPath.row].done = !itemArray[indexPath.row].done
                  saveItems()
             tableView.deselectRow(at: indexPath, animated: true)
